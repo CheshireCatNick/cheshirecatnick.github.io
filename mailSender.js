74f4ffe314e70a62247abf3766c7ec597161cdeb
cheshirecatnick <cheshirecatnick@gmail.com>
2016-08-12 03:22:21 +0800

    add contact; finish a quick email system

--- mailSender.js
+++ mailSender.js
@@ -3,27 +3,29 @@ var port = 2147;
 
 var nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport("smtps://cheshirecatnick%40gmail.com:st55330066@smtp.gmail.com");
-var email = {
-	from: '"Nicky" <st945306@gmail.com>',
-	to: 'cheshirecatnick@gmail.com',
-	text: 'succeed!'
-};
-/*
-transporter.sendMail(email, function(err, info){
-	if (err)
-		console.log("err");
-	else
-		console.log("email sent!");
-});
-*/
+
+
+
+
 var server = require("http").createServer();
 server.listen(port, ip);
 var io = require("socket.io").listen(server);
 console.log("server is running on " + ip + ":" + port);
 io.sockets.on("connection", function(socket){
-	socket.on("sendEmail", function(email){
-		console.log("got a mail");		
-	
+	socket.on("sendEmail", function(content){
+		var email = {
+			to: "cheshirecatnick@gmail.com",
+			text: content
+		};
+		transporter.sendMail(email, function(err, info){
+			if (err)
+				console.log("err");
+			else {
+				console.log("Receive an email from ???");
+				console.log(content);
+				console.log("Email was sent to CheshireCatNick");
+			}
+		});
 	});
 });
 

f77572bbbc378d29544898d0628bf18c3240cbd4
cheshirecatnick <cheshirecatnick@gmail.com>
2016-08-12 13:49:13 +0800

    add name and email address

---
+++ js/animation.js
@@ -0,0 +1,38 @@
+var timer;
+function addWidth(C, J, P, H, CDelta, JDelta, PDelta, HDelta, maxC){
+	var	CWidth = parseFloat(C.style.width);
+	var	JWidth = parseFloat(J.style.width);
+	var	PWidth = parseFloat(P.style.width);
+	var	HWidth = parseFloat(H.style.width);
+	if (CWidth >= maxC){
+		clearInterval(timer);
+		return;
+	}
+	C.style.width = CWidth + CDelta + "%";
+	J.style.width = JWidth + JDelta + "%";
+	P.style.width = PWidth + PDelta + "%";
+	H.style.width = HWidth + HDelta + "%";
+}
+
+function moveProgressBar(){
+	var C = document.getElementById("C");
+	var J = document.getElementById("java");
+	var P = document.getElementById("python");
+	var H = document.getElementById("html");
+
+	var maxC = 90, maxJ = 90, maxP = 80, maxH = 70;
+	
+	var animationTime = 4000;
+	var addWidthPeriod = 30;
+	var addWidthNum = animationTime / addWidthPeriod;
+	var CDelta = maxC / addWidthNum;
+	var JDelta = maxJ / addWidthNum;
+	var PDelta = maxP / addWidthNum;
+	var HDelta = maxH / addWidthNum;
+
+	C.style.width = "1%";
+	J.style.width = "1%";
+	P.style.width = "1%";
+	H.style.width = "1%";
+	timer = setInterval(function(){addWidth(C, J, P, H, CDelta, JDelta, PDelta, HDelta, maxC);}, addWidthPeriod);
+}
--- mailSender.js
+++ mailSender.js
@@ -4,24 +4,23 @@ var port = 2147;
 var nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport("smtps://cheshirecatnick%40gmail.com:st55330066@smtp.gmail.com");
 
-
-
-
 var server = require("http").createServer();
 server.listen(port, ip);
 var io = require("socket.io").listen(server);
 console.log("server is running on " + ip + ":" + port);
 io.sockets.on("connection", function(socket){
-	socket.on("sendEmail", function(content){
+	socket.on("sendEmail", function(name, emailAddress, content){
+		var info = "from " + name + " <" + emailAddress + ">";
 		var email = {
 			to: "cheshirecatnick@gmail.com",
-			text: content
+			subject: "You got an email from your websites!",
+			text: info + "\n\n" + content
 		};
-		transporter.sendMail(email, function(err, info){
+		transporter.sendMail(email, function(err, errInfo){
 			if (err)
 				console.log("err");
 			else {
-				console.log("Receive an email from ???");
+				console.log("Receive an email " + info);
 				console.log(content);
 				console.log("Email was sent to CheshireCatNick");
 			}
